eaf02e84871c2a5a2ea8552cc5cee3d1bed8af7d
anujpanchal57 <anuj.panchal@somaiya.edu>
2017-02-25 15:34:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,8 +28,7 @@ button.onclick = function() {
 
 // Submit name
 
-var nameInput = document.getElementById('Name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
   // Make a request to the server and send the name
@@ -59,6 +58,8 @@ submit.onclick = function() {
     };
     
     // Make the request
+        var nameInput = document.getElementById('Name');
+        var name = nameInput.value;
         request.open('GET','http://anujpanchal57.imad.hasura-app.io/submit-name?name=' + name,true);
         request.send(null);
   
