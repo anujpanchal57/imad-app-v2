3063146dc751adc92cae32b496a3ebcc22a3362b
anujpanchal57 <anuj.panchal@somaiya.edu>
2017-03-09 20:08:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/test-db', function(req, res){
     // make a select request
     // return the response with the results
     pool.query('SELECT * FROM test', function(err, result){
-        if(err){
+        if(err) {
             res.status(500).send(err.toString());
         } else {
             res.send(JSON.stringify(result));
